02d4e0a7724ba8b0f5cf840b96ee31fed11dde43
Alex Osendi <alejandro.osendi@doubledot.es>
2023-10-17 00:26:18 +0200

    Added the center button

--- js/src/forum/components/AddButtons.js
+++ js/src/forum/components/AddButtons.js
@@ -4,6 +4,17 @@ import TextEditorButton from 'flarum/components/TextEditorButton';
 
 export default function () {
     extend(TextEditor.prototype, 'toolbarItems', function (items) {
+        items.add('center', (
+            <TextEditorButton onclick={() => {
+                let selection = this.attrs.composer.editor.getSelectionRange();
+                this.attrs.composer.editor.insertAt(selection[1], '[/center]');
+                this.attrs.composer.editor.insertAt(selection[0], '[center]');
+            }} icon="fas fa-align-center"
+            >
+                {app.translator.trans('v17development-flarum-blog.forum.buttons.center_tooltip')}
+            </TextEditorButton>
+        ),100);
+
         items.add('score', (
             <TextEditorButton onclick={() => {
                 this.attrs.composer.editor.insertAtCursor('[score]50[/score]');
@@ -11,15 +22,17 @@ export default function () {
             >
                 {app.translator.trans('v17development-flarum-blog.forum.buttons.score_tooltip')}
             </TextEditorButton>
-        ));
+        ),100);
 
         items.add('review', (
             <TextEditorButton onclick={() => {
-                this.attrs.composer.editor.insertAtCursor('[review] :D [/review]');
+                let selection = this.attrs.composer.editor.getSelectionRange();
+                this.attrs.composer.editor.insertAt(selection[1], '[/review]');
+                this.attrs.composer.editor.insertAt(selection[0], '[review]');
             }} icon="far fa-comment"
             >
                 {app.translator.trans('v17development-flarum-blog.forum.buttons.review_tooltip')}
             </TextEditorButton>
-        ));
+        ),100);
     });
 }
